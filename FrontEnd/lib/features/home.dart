c7ac08e6adc150b6f550f508ecbb9ad6ab74a9d6
4F5DA2 <E0969244@u.nus.edu>
2023-05-12 21:20:34 +0800

    test

--- FrontEnd/lib/features/home.dart
+++ FrontEnd/lib/features/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:supercellmates/router/router.gr.dart';
 
+import 'dart:io';
+import 'dart:convert';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -17,18 +20,23 @@ class HomePageState extends State<HomePage> {
         AppBar(
           actions: [
             IconButton(
-              onPressed: () =>
-                  AutoRouter.of(context).push(const FriendRequestRoute()),
+              onPressed: () async {
+                HttpClient client = HttpClient();
+                HttpClientRequest request = await client.get('127.0.0.1', 8000, '/');
+                HttpClientResponse response = await request.close();
+                final stringData = await response.transform(utf8.decoder).join();
+                print(stringData);
+              },
+
+              //AutoRouter.of(context).push(const FriendRequestRoute()),
               icon: const Icon(Icons.people),
               iconSize: 25,
             ),
-            Container(padding: 
-              const EdgeInsets.all(10)
-            ),
+            Container(padding: const EdgeInsets.all(10)),
           ],
         ),
-        Expanded(child:
-          Container(
+        Expanded(
+          child: Container(
             height: 100,
             alignment: Alignment.center,
             child: const Text("Home page body"),
