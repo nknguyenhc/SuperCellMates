c968b97a7718e2056ba5c68a04ff61cd6a13e7d9
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-05-13 18:28:47 +0800

    Login and out
    Created login and logout views, both for redirecting and asynchronous requests

--- supercell_mates/user_auth/urls.py
+++ supercell_mates/user_auth/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = "user_auth"
 urlpatterns = [
     path('', views.home, name="home"),
+    path('async', views.home_async, name="home_async"),
     path('login', views.login_user, name="login"),
-    path('register', views.register, name="register")
+    path('login_async', views.login_async, name="login_async"),
+    path('register', views.register, name="register"),
+    path('register_async', views.register_async, name="register_async"),
+    path('logout', views.logout_user, name="logout"),
+    path('logout_async', views.logout_async, name="logout_async")
 ]
--- supercell_mates/user_auth/views.py
+++ supercell_mates/user_auth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.http import HttpResponse, JsonResponse
+from json import loads
 from .models import UserAuth
 
 # Create your views here.
@@ -9,7 +11,28 @@ def home(request):
     if not request.user.is_authenticated:
         return redirect(reverse("user_auth:login"))
     else:
-        return render(request, "home.html")
+        return render(request, "user_auth/home.html")
+
+
+def home_async(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({"message": "not logged in"})
+    else:
+        return JsonResponse({"message": "logged in"})
+
+
+def login_async(request):
+    if request.method == "POST":
+        data = loads(request.body.decode('utf-8'))
+        username = data["username"]
+        password = data["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": "logged in"})
+        else:
+            return JsonResponse({"message": "wrong username or password"})
 
 
 def login_user(request):
@@ -22,11 +45,26 @@ def login_user(request):
             login(request, user)
             return redirect(reverse("user_auth:home"))
         else:
-            return render(request, "user_auth/login.html", {
+            return render(request, 'user_auth/login.html', {
                 "error_message": "Wrong username or password"
             })
-
+    
     return render(request, "user_auth/login.html")
+
+
+def register_async(request):
+    if request.method == "POST":
+        data = loads(request.body.decode('utf-8'))
+        username = data["username"]
+        password = data["password"]
+
+        try:
+            user = UserAuth.objects.create_user(username=username, password=password)
+            login(request, user)
+        except IntegrityError:
+            return JsonResponse({"message": "username already taken"})
+        
+        return JsonResponse({"message": "account created"})
 
 
 def register(request):
@@ -39,15 +77,19 @@ def register(request):
             login(request, user)
         except IntegrityError:
             return render(request, "user_auth/register.html", {
-                "error_message": "Username already taken"
+                "error_message": "username already taken"
             })
         
         return redirect(reverse("user_auth:home"))
 
-    else:
-        return render(request, "user_auth/register.html")
+    return render(request, "user_auth/register.html")
 
 
 def logout_user(request):
     logout(request)
     return redirect(reverse("user_auth:home"))
+
+
+def logout_async(request):
+    logout(request)
+    return JsonResponse({"message": "logged out"})
