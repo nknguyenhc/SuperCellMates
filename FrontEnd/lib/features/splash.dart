92d045a8036ba88b5c8a13ae4e6e1d74db955d6a
4F5DA2 <E0969244@u.nus.edu>
2023-05-14 22:35:30 +0800

    splash page

---
+++ FrontEnd/lib/features/splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+
+  //TODO
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
