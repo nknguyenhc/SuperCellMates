67416393d8e6b7ae25d35b1e199a7422642eb464
4F5DA2 <E0969244@u.nus.edu>
2023-05-24 22:40:23 +0800

    profile page posts

--- FrontEnd/lib/features/profile/profile.dart
+++ FrontEnd/lib/features/profile/profile.dart
@@ -20,36 +20,51 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     var tagList = widget.data["tagListString"].split(";");
+    double myPostsHeight = MediaQuery.of(context).size.height;
+    myPostsHeight -= 80; // appbar height
+    myPostsHeight -= 60; // taglist height
+    myPostsHeight -= 10; // divider height
+    myPostsHeight -= 66; // createPost height
+    myPostsHeight -= 82; // bottom navigation bar height
+    myPostsHeight -= 30; // buffer
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         SizedBox(
-          height: 70,
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: tagList.length + 1,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return index < tagList.length
-                    ? tagList[index] == ""
-                        ? Container()
-                        : TextButton(
-                            onPressed: () => {}, child: Text(tagList[index]))
-                    : IconButton(
-                        onPressed: () => AutoRouter.of(context).push(
-                            AddTagRoute(updateCallBack: widget.updateCallBack)),
-                        icon: const Icon(Icons.add_circle_outline_rounded),
-                        iconSize: 50,
-                      );
-              }),
+          height: 60,
+          child: Flex(direction: Axis.horizontal, children: [
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: tagList.length + 1,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    return index < tagList.length
+                        ? tagList[index] == ""
+                            ? Container()
+                            : TextButton(
+                                onPressed: () => {},
+                                child: Text(tagList[index]))
+                        : IconButton(
+                            onPressed: () => AutoRouter.of(context).push(
+                                AddTagRoute(
+                                    updateCallBack: widget.updateCallBack)),
+                            icon: const Icon(Icons.add_circle_outline_rounded),
+                            iconSize: 50,
+                          );
+                  }),
+            )
+          ]),
         ),
         const Divider(
+          height: 10,
           color: Colors.grey,
           indent: 15,
           endIndent: 15,
         ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
               icon: const Icon(Icons.note_add, size: 50),
@@ -57,23 +72,25 @@ class ProfilePageState extends State<ProfilePage> {
             )
           ],
         ),
-        Column(
-          children: [
-            // TODO: Change to Posts class
-            SizedBox(
-              height: 400,
-              width: 100,
-              child: ListView.builder(
-                itemCount: 30,
-                itemBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    child: Text("Post Entry $index"),
-                  );
-                },
-              ),
-            )
-          ],
-        )
+        // TODO: Change to Posts class
+        // The Posts should return a column whose width is full width of phone
+        // and pass this column to a flex expanded so that can scroll down
+        SizedBox(
+          height: myPostsHeight,
+          width: MediaQuery.of(context).size.width,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: 30,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                width: 100,
+                height: 30,
+                child: Text("Post Entry $index"),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
