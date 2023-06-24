d357c5b6a2894b94cc2271df95c8ce2b941e503e
4F5DA2 <E0969244@u.nus.edu>
2023-06-25 00:06:08 +0800

    one word!!!!!!!!!

--- FrontEnd/android/app/src/main/kotlin/com/supercellmates/supercellmates/MainActivity.kt
+++ FrontEnd/android/app/src/main/kotlin/com/supercellmates/supercellmates/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.supercellmates.supercellmates
+package com.supercellmates.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- FrontEnd/lib/locator.dart
+++ FrontEnd/lib/locator.dart
@@ -1,13 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:supercellmates/config/config.dart';
-import 'package:supercellmates/config/dev_config.dart';
-//import 'package:supercellmates/config/prod_config.dart';
+//import 'package:supercellmates/config/dev_config.dart';
+import 'package:supercellmates/config/prod_config.dart';
 import 'package:supercellmates/router/router.dart';
 
 GetIt locator = GetIt.I;
 
 void setupLocator() {
-  locator.registerLazySingleton<Config>(() => DevConfig());
-  //locator.registerLazySingleton<Config>(() => ProdConfig());
+  //locator.registerLazySingleton<Config>(() => DevConfig());
+  locator.registerLazySingleton<Config>(() => ProdConfig());
   locator.registerSingleton<AppRouter>(AppRouter());
 }
