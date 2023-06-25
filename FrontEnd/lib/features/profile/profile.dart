213219bb5a789bb7a08acfadd939317684246278
4F5DA2 <E0969244@u.nus.edu>
2023-06-25 23:36:34 +0800

    hm

--- FrontEnd/lib/features/profile/profile.dart
+++ FrontEnd/lib/features/profile/profile.dart
@@ -2,7 +2,6 @@ import 'dart:typed_data';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:supercellmates/features/dialogs.dart';
 import 'package:supercellmates/features/posts/post_listview.dart';
 import 'dart:convert';
--- FrontEnd/lib/features/profile/search_tag_appbar.dart
+++ FrontEnd/lib/features/profile/search_tag_appbar.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
-import 'package:auto_route/auto_route.dart';
-import 'package:supercellmates/router/router.gr.dart';
+
 import 'package:flutter/material.dart';
 import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:supercellmates/features/home/search.dart';
