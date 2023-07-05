7712d2483abd06aa9e4e8d7de1b7bb5b995b18f0
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-07-05 20:50:48 +0800

    Updated Postgre Config

--- supercell_mates/supercell_mates/settings.py
+++ supercell_mates/supercell_mates/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +94,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
