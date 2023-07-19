0b70299aa2b913ae756ee423159fdeb3aa6981d4
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-07-19 22:55:46 +0800

    Search UI-UX
    search query is sent when user is idle in the input box, not just on pressing enter/search

--- supercell_mates/static/script/user_log/search-bar.js
+++ supercell_mates/static/script/user_log/search-bar.js
@@ -5,27 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (searchForm !== null) {
         searchForm.addEventListener('submit', event => {
             event.preventDefault();
-            if (searchField.value !== '') {
-                if (searchField.value[0] === '@') {
-                    fetch('/user/search_username?username=' + searchField.value.slice(1))
-                        .then(response => {
-                            if (response.status !== 200) {
-                                triggerErrorMessage();
-                            } else {
-                                response.json().then(response => displayResult(response.users))
-                            }
-                        });
-                } else {
-                    fetch('/user/search?username=' + searchField.value)
-                        .then(response => {
-                            if (response.status !== 200) {
-                                triggerErrorMessage();
-                            } else {
-                                response.json().then(response => displayResult(response.users))
-                            }
-                        });
-                }
-            }
+            searchUser();
         });
         searchField.addEventListener('focus', () => {
             if (searchResultBox.style.display === "none") {
@@ -39,5 +19,43 @@ document.addEventListener("DOMContentLoaded", () => {
                 removeSearchBox();
             }
         });
+
+        let currTimeout = null;
+        let isSearching = false;
+        searchField.addEventListener('input', () => {
+            clearTimeout(currTimeout);
+            currTimeout = setTimeout(() => {
+                if (searchField === document.activeElement) {
+                    searchUser();
+                }
+            }, 800);
+        })
+
+        function searchUser() {
+            if (searchField.value !== '' && !isSearching) {
+                isSearching = true;
+                if (searchField.value[0] === '@') {
+                    fetch('/user/search_username?username=' + searchField.value.slice(1))
+                        .then(response => {
+                            isSearching = false;
+                            if (response.status !== 200) {
+                                triggerErrorMessage();
+                            } else {
+                                response.json().then(response => displayResult(response.users))
+                            }
+                        });
+                } else {
+                    fetch('/user/search?username=' + searchField.value)
+                        .then(response => {
+                            isSearching = false;
+                            if (response.status !== 200) {
+                                triggerErrorMessage();
+                            } else {
+                                response.json().then(response => displayResult(response.users))
+                            }
+                        });
+                }
+            }
+        }
     }
 });
