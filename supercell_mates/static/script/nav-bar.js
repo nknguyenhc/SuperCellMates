f4dec8db6510e4449a8eb689d3306e59347f3f4e
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-07-20 13:46:01 +0800

    Nav Bar Revamp

--- supercell_mates/static/script/nav-bar.js
+++ supercell_mates/static/script/nav-bar.js
@@ -4,3 +4,17 @@ if (requestTagLink !== null) {
         document.querySelector("#add_tag").style.display = "block";
     });
 }
+
+(() => {
+    if (window.location.pathname === '/') {
+        document.querySelector("#nav-home").classList.add('bg-info');
+    } else if (window.location.pathname.startsWith('/profile')) {
+        document.querySelector("#nav-profile").classList.add('bg-info');
+    } else if (window.location.pathname.startsWith('/messages')) {
+        document.querySelector("#nav-message").classList.add('bg-info');
+    } else if (window.location.pathname.startsWith('/manage_page')) {
+        document.querySelector("#nav-admin").classList.add('bg-info');
+    } else if (window.location.pathname.startsWith('/settings')) {
+        document.querySelector("#nav-settings").classList.add('bg-info');
+    }
+})();
