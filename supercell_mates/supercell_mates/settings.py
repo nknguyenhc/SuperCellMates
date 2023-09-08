b7aae4c20cce5325e3b21a1eea91ae9460d01db4
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-09-08 11:42:00 +0800

    Fix port error

--- supercell_mates/supercell_mates/settings.py
+++ supercell_mates/supercell_mates/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("PORT"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
