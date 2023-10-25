797ad78d6966ec2fff298142f2b624d8911feb4c
mman <minhmanuni@gmail.com>
2023-10-25 20:17:35 +0800

    build profile dashboard

--- webfrontend/src/components/profile/ProfileDashBoard.tsx
+++ webfrontend/src/components/profile/ProfileDashBoard.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './ProfileDashBoard.scss'
+import {FcInvite} from 'react-icons/fc'
+import {BsFillPeopleFill} from 'react-icons/bs'
 const ProfileDashBoard = () => {
   return (
     <div className='profile-dash-board'>
-      profile dashboard
+      <div className="dashboard-container">
+        <div className="left-section">
+          <div className="option">
+              <FcInvite/> 
+              Friend Requests
+            </div>
+
+          <div className="option">
+            <BsFillPeopleFill/> 
+            Friends
+          </div>
+        </div>
+        <div className="right-section">right section</div>
+      </div>
+      
     </div>
   )
 }
--- webfrontend/src/pages/profile/Profile.tsx
+++ webfrontend/src/pages/profile/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProfileWallpaper from '../../components/profile/ProfileWallpaper'
+import ProfileDashBoard from '../../components/profile/ProfileDashBoard'
 const Profile = () => {
   return (
     <div className='profile-page'>
       <ProfileWallpaper/>
+      <ProfileDashBoard />
     </div>
   )
 }
