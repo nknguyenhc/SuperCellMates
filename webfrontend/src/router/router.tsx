402d21db6de1518f9786677d34ec7b886ae470cb
mman <minhmanuni@gmail.com>
2023-10-25 19:42:58 +0800

    create profile wallpaper

--- webfrontend/src/router/router.tsx
+++ webfrontend/src/router/router.tsx
@@ -4,7 +4,6 @@ import { Login } from "../components/auth/login/login";
 import Home from "../pages/home/home";
 import Messages from "../pages/messages/messages";
 import Settings from "../pages/settings/settings";
-import Profile from "../pages/profile/Profile";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -25,10 +24,6 @@ const router = createBrowserRouter([
             {
                 path: "/settings",
                 element: <Settings />
-            },
-            {
-                path: "/profile",
-                element: <Profile />
             }
         ]
     }
