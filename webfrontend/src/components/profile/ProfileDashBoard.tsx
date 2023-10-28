51511d313fe4841f8eef0c21a764f672af5057f1
mman <minhmanuni@gmail.com>
2023-10-28 20:21:42 +0800

    display friend requests

--- webfrontend/src/components/profile/ProfileDashBoard.tsx
+++ webfrontend/src/components/profile/ProfileDashBoard.tsx
@@ -11,6 +11,7 @@ export type FriendType = {
 }
 const ProfileDashBoard = () => {
   const [currentFriends, setCurrentFriends] = useState<Array<FriendType>>([]);
+  const [friendRequests,setFriendRequests] = useState<Array<FriendType>>([]);
   const [buttonClick, setButtonClick] = useState<number>(1);
   const [currClass, setCurrClass] = useState<string>("first");
   const handleMenuClick = useCallback((id: number, name: string)=> {
@@ -18,10 +19,8 @@ const ProfileDashBoard = () => {
     const prev = document.getElementById(`${currClass}`);
 
     prev?.classList.remove('clicked-button');
-    console.log(prev);
     const cur = document.getElementById(`${name}`);
     cur?.classList.add('clicked-button');
-    console.log(cur);
     setButtonClick(id);
     setCurrClass(name);
 
@@ -34,20 +33,41 @@ const ProfileDashBoard = () => {
           return;
         }
         res.json().then(res => {
-          setCurrentFriends(res.users.map((user:any) =>({
+          setCurrentFriends(res.users?.map((user:any) =>({
             name: user.name,
             username: user.username,
             link: user.profile_link,
             img: user.profile_img_url,
             role : user.role,
           })));
-          console.log(currentFriends);
         })
       })
   },[])
   useEffect(() => {
     getCurrentFriends();
   },[getCurrentFriends]); 
+
+  const getFriendRequest = useCallback(() =>{
+    fetch('/user/friend_requests_async')
+      .then(res => {
+        if (res.status !== 200) {
+          triggerErrorMessage();
+          return;
+        }
+        res.json().then(res => {
+          setFriendRequests(res?.map((user:any) =>({
+            name: user.name,
+            username: user.username,
+            link: user.profile_link,
+            img: user.profile_pic_url,
+          }))); 
+        })
+        console.log(friendRequests);
+      })
+  },[]);
+  useEffect(() => {
+    getFriendRequest();
+  },[getFriendRequest]); 
   return (
     <div className='profile-dash-board'>
       <div className="dashboard-container">
@@ -77,32 +97,37 @@ const ProfileDashBoard = () => {
               <li>{friends?.name}</li>
           ))} */}
           <li className='friend-info'>
-            <img src="/Jiale.jpg" className='friend-thumbnail' />
-            <p className='friend-name'> Jiale</p>  
+            <img src="/default_profile_pic.jpg" className='friend-thumbnail' />
+            <p className='friend-name'> Man</p>  
           </li> 
           <li className='friend-info'>
-            <img src="/Jiale.jpg" className='friend-thumbnail' />
-            <span className='friend-name'> Jiale</span>
+            <img src="/default_profile_pic.jpg" className='friend-thumbnail' />
+            <span className='friend-name'> Man</span>
           </li> 
           </ul>}
           {
             buttonClick == 1 &&
             <ul className='friend-list'>
-            {/*} {currentFriends.map((friends) => (
-                <li>{friends?.name}</li>
-            ))} */}
-            <li className='friend-info'>
-              <img src="/Jiale.jpg" className='friend-thumbnail' />
-              <p className='friend-name'> Jiale</p>  
+             {friendRequests?.map((friends,id) => (
+                <li key={id} className='friend-info'>
+                <img src="/default_profile_pic.jpg" className='friend-thumbnail' />
+                <p className='friend-name'> {friends.name}</p>  
+                <Button className='approve-btn' variant='success'> Aprrove</Button>
+                <Button className='reject-btn' variant='danger'> Reject</Button>
+              </li> 
+            ))} 
+            {/*<li className='friend-info'>
+              <img src="/default_profile_pic.jpg" className='friend-thumbnail' />
+              <p className='friend-name'> Man</p>  
               <Button className='approve-btn' variant='success'> Aprrove</Button>
               <Button className='reject-btn' variant='danger'> Reject</Button>
             </li> 
             <li className='friend-info'>
-              <img src="/Jiale.jpg" className='friend-thumbnail' />
-              <span className='friend-name'> Jiale</span>
+              <img src="/default_profile_pic.jpg" className='friend-thumbnail' />
+              <span className='friend-name'> man</span>
               <Button className='approve-btn' variant='success'> Aprrove</Button>
               <Button className='reject-btn' variant='danger'> Reject</Button>
-            </li> 
+             </li> */}
             </ul>
 
           }
