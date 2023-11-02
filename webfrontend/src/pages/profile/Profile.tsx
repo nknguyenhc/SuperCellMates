8a1a003c8fac55321d55611e6bf62fbfd4758c1b
mman <minhmanuni@gmail.com>
2023-11-02 22:02:29 +0800

    add about link

--- webfrontend/src/pages/profile/Profile.tsx
+++ webfrontend/src/pages/profile/Profile.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { AiOutlineQuestionCircle } from 'react-icons/ai'
 import ProfileWallpaper from '../../components/profile/ProfileWallpaper'
 import ProfileDashBoard from '../../components/profile/ProfileDashBoard'
+import { Link } from 'react-router-dom'
 const Profile = () => {
   return (
     <div className='profile-page'>
       <ProfileWallpaper/>
       <ProfileDashBoard />
+      <Link to='/about' className='about-link'>
+            <AiOutlineQuestionCircle/>   About </Link>
+
     </div>
   )
 }
