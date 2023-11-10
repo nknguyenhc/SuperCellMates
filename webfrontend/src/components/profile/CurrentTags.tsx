0197b77d3e3f5c4ff93bee48958a0bd0a34e9b90
mman <minhmanuni@gmail.com>
2023-11-10 22:37:02 +0800

    update tags menu

--- webfrontend/src/components/profile/CurrentTags.tsx
+++ webfrontend/src/components/profile/CurrentTags.tsx
@@ -6,70 +6,34 @@ interface Props {
   username: string
 }
 const CurrentTags:React.FC<Props> = ({username}) => {
-  const [newTag, setNewTag] = useState<string>('');
-  const [userTags, setUserTags] = useState<Array<Tag>>([]);
-  const getTags = useCallback(() => {
+  const [tags, setTags] = useState<Array<Tag>>([]);
+  useEffect(() => {
     fetch('/profile/obtain_tags')
-      .then (res => {
-        if (res.status !== 200) {
-          triggerErrorMessage();
-          return;
-        } 
-      })
-  }, []);
-  useEffect(() => {
-    getTags();
-  }, [getTags])
-  const handleFindTag = useCallback(() => {
-    fetch(`/profile/search_tags?tag=${newTag}`)
-      .then(res => {
-        if (res.status !== 200) {
-          triggerErrorMessage();
-          return;
-        }
-      })
-  }, [newTag]);
+        .then(response => {
+            if (response.status !== 200) {
+                triggerErrorMessage();
+            } else {
+                response.json()
+                    .then(response => {
+                        setTags(response.tags);
+                        console.log(tags);
+                    });
+            }
+        });
+    
+    }, []);
 
-  useEffect(() => {
-    if (username !== '') {
-        fetch('/profile/user_tags/' + username)
-            .then(response => {
-                if (response.status !== 200) {
-                    triggerErrorMessage();
-                } else {
-                    response.json().then(response => setUserTags(response.tags as Array<Tag>));
-                }
-            });
-    }
-}, [username, userTags]);
   return (
     <div className='current-tag-container'>
-        <div className='tag-list'>
-            {userTags.length !== 0 ?  userTags.map((tag) => (
-              <div className='tag-info'>
-                <img className='tag-icon' src={tag.icon} alt={tag.name} />
-                <p className='tag-title'>{tag.name}</p>
-               
-
-              </div>
-            )) : "" }
-        </div>
-        <div className='tag-edit'>
-          <input 
-            type='text'
-            placeholder='Enter a tag'
-            onChange={(e) => setNewTag(e.target.value)}
-            className='form-control newtag-input-field'
-          />
-          <Button
-              className='find-tag-btn'
-              onClick = {() => handleFindTag()}          
-          >
-            Find Tag
-          </Button>
-        </div>
-        
-      
+      <div className="tag-title">Your Current Tags</div>
+      <div className="tag-list">
+          {tags.map(tag => (
+                  <div className="tag-button btn btn-outline-info">
+                      <img src={tag.icon} />
+                      <div>{tag.name}</div>
+                  </div>
+          ))}
+      </div>
     </div>
   )
 }
