967c423c30906c7304e9466c582697c626b03a19
mman <minhmanuni@gmail.com>
2023-11-17 20:32:44 +0800

    update

--- webfrontend/src/router/router.tsx
+++ webfrontend/src/router/router.tsx
@@ -4,6 +4,7 @@ import { Login } from "../components/auth/login/login";
 import { Register } from "../components/auth/register/register";
 import Home from "../pages/home/home";
 import Messages from "../pages/messages/messages";
+import Profile from "../pages/profile/Profile";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
             {
                 path: "/messages",
                 element: <Messages />
+            },
+            {
+                path: "/profile",
+                element: <Profile/>
             }
         ]
     }
