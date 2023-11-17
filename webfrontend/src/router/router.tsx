19ed15baf491fd0fb9efe3b917f39e2a80426720
mman <minhmanuni@gmail.com>
2023-11-17 21:12:51 +0800

    resolve merge conflicts

--- webfrontend/src/router/router.tsx
+++ webfrontend/src/router/router.tsx
@@ -5,6 +5,7 @@ import { Register } from "../components/auth/register/register";
 import Home from "../pages/home/home";
 import Messages from "../pages/messages/messages";
 import Profile from "../pages/profile/Profile";
+import OtherProfileWallpaper from "../components/profile/OtherProfileWallpaper";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
             {
                 path: "/profile",
                 element: <Profile/>
+            },
+            {
+                path:"/user/profile/:username",
+                element: <OtherProfileWallpaper/>
             }
         ]
     }
