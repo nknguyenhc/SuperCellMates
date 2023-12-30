29bf09fb216370501c350793c3760871c00d8aa9
mman <minhmanuni@gmail.com>
2023-12-30 15:47:39 +0700

    update request-tag-modal

--- webfrontend/src/components/navbar/navbar.tsx
+++ webfrontend/src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { RootState } from '../../redux/store';
 import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import RequestTagModal from "../popup/request-tag-modal";
 
 type PublicRoute = {
     href: string,
@@ -43,47 +44,67 @@ export default function NavBar(): JSX.Element {
             text: 'Register',
         },
     ], []);
-    const loggedInRoutes = useMemo<Array<LoggedInRoute>>(() => [
+    const loggedInRoutes = useMemo<Array<LoggedInRoute>>(
+      () => [
         {
-            href: '/',
-            image: 'home.png',
-            text: 'Home',
-            id: 'nav-home'
+          href: "/",
+          image: "home.png",
+          text: "Home",
+          id: "nav-home",
         },
         {
-            image: 'request-tag.png',
-            text: 'Request tag',
-            id: 'nav-request-tag',
+          image: "request-tag.png",
+          text: "Request tag",
+          id: "nav-request-tag",
+          children: (
+            <RequestTagModal />
+          ),
         },
         {
-            href: '/profile',
-            image: 'profile.png',
-            text: 'Profile',
-            id: 'nav-profile',
+          href: "/profile",
+          image: "profile.png",
+          text: "Profile",
+          id: "nav-profile",
         },
         {
-            href: '/messages',
-            image: 'message.png',
-            text: 'Message',
-            id: 'nav-message',
-            children: <span className="position-absolute top-100 start-100 translate-middle badge rounded-pill bg-danger" id="message-count-badge" />,
+          href: "/messages",
+          image: "message.png",
+          text: "Message",
+          id: "nav-message",
+          children: (
+            <span
+              className="position-absolute top-100 start-100 translate-middle badge rounded-pill bg-danger"
+              id="message-count-badge"
+            />
+          ),
         },
         {
-            image: 'notification-icon.png',
-            text: 'Notification',
-            id: 'nav-notification',
-            children: <>
-                <span className="position-absolute top-100 start-100 translate-middle badge rounded-pill bg-danger" id="notification-count-badge" />
-                <div className="position-absolute" id="notification-centre" style={{ display: "none" }} />
-            </>,
+          image: "notification-icon.png",
+          text: "Notification",
+          id: "nav-notification",
+          children: (
+            <>
+              <span
+                className="position-absolute top-100 start-100 translate-middle badge rounded-pill bg-danger"
+                id="notification-count-badge"
+              />
+              <div
+                className="position-absolute"
+                id="notification-centre"
+                style={{ display: "none" }}
+              />
+            </>
+          ),
         },
         {
-            href: '/settings',
-            image: 'settings.png',
-            text: 'Settings',
-            id: 'nav-settings',
+          href: "/settings",
+          image: "settings.png",
+          text: "Settings",
+          id: "nav-settings",
         },
-    ], []);
+      ],
+      []
+    );
     const adminRoutes = useMemo<Array<AdminRoute>>(() => [
         {
             href: '/manage_page',
@@ -122,6 +143,7 @@ export default function NavBar(): JSX.Element {
                                 data-bs-placement="bottom" 
                                 data-bs-title={route.text}
                                 key={routeIndex}
+                            
                             >
                                 {route.href 
                                 ? <Link to={route.href}>
@@ -129,8 +151,9 @@ export default function NavBar(): JSX.Element {
                                         <img src={process.env.PUBLIC_URL + '/media/nav-bar/' + route.image} alt={route.text} />
                                     </div>
                                 </Link>
-                                : <div className="nav-link">
-                                    <img src={process.env.PUBLIC_URL + '/media/nav-bar/' + route.image} alt={route.text} />
+                                : <div className="nav-link" onClick={() => {document.querySelector<HTMLElement>(`#${route.id}`)!.style.display = "block"}}>
+                                    <img src={process.env.PUBLIC_URL + '/media/nav-bar/' + route.image} alt={route.text}  onClick={() => {document.querySelector<HTMLElement>(`#request-tag`)!.style.display = "block"}} />
+                                   
                                 </div>}
                                 {route.children && route.children}
                             </li>
@@ -145,7 +168,7 @@ export default function NavBar(): JSX.Element {
                                 data-bs-title={route.text}
                                 key={routeIndex}
                             >
-                                <Link to={route.href} className="nav-link">
+                                <Link to={route.href} className="nav-link" >
                                     <img src={process.env.PUBLIC_URL + '/media/nav-bar/' + route.image} alt={route.text} />
                                 </Link>
                             </li>
--- webfrontend/src/components/popup/request-tag-modal.tsx
+++ webfrontend/src/components/popup/request-tag-modal.tsx
@@ -1,42 +1,36 @@
-import { useCallback, useRef, useState } from "react";
+import { SyntheticEvent, useCallback, useRef, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { triggerErrorMessage } from "../../utils/locals";
 import { postRequestContent } from "../../utils/request";
 
-interface Props {
-  imgLink: string;
-}
 
-const RequestTagModal: React.FC<Props> = ({ imgLink }) => {
-  const [tag, setTag] = useState("");
+const RequestTagModal: React.FC = () => {
+  const [tag, setTag] = useState<string>("");
   const tagInput = useRef<HTMLInputElement>(null);
-  const [errMessage, setErrMessage] = useState("");
+  const [errMessage, setErrMessage] = useState<string>("");
   const [imagePreview, setImagePreview] = useState<string>("");
-  const [imageToBeSubmitted, setImageToBeSubmitted] = useState<string>("");
+  const [imageToBeSubmitted, setImageToBeSubmitted] = useState<File>();
   const imageInput = useRef<HTMLInputElement>(null);
-  const [description, setDescription] = useState("");
-  const [attach, setAttach] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [description, setDescription] = useState<string>("");
+  const [attach, setAttach] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [tagRequestMessageContent, setTagRequestMessageContent] =
     useState<string>("");
-  const [show, setShow] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
+  const [showMessage, setShowMessage] = useState<boolean>(false);
 
-  const handleAddImage = useCallback((event: React.ChangeEvent<any>) => {
-    setImagePreview(URL.createObjectURL(event.target.files[0]));
-    setImageToBeSubmitted(event.target.files[0]);
+  const handleAddImage = useCallback((event: SyntheticEvent) => {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    setImagePreview(URL.createObjectURL(file));
+    setImageToBeSubmitted(file);
   }, []);
 
   const handleClose = useCallback(() => {
-    setShow(false);
+    document.querySelector<HTMLElement>("#request-tag")!.style.display = "none";
     setErrMessage("");
   }, []);
 
-  const handleShow = useCallback(() => {
-    setShow(true);
-    setErrMessage("");
-  }, []);
 
   const submitForm = useCallback(
     (event: React.SyntheticEvent<EventTarget>) => {
@@ -54,7 +48,7 @@ const RequestTagModal: React.FC<Props> = ({ imgLink }) => {
         description: description,
         attach: attach,
       };
-      if (imageToBeSubmitted !== "") {
+      if (imageToBeSubmitted !== undefined) {
         requestBody.img = imageToBeSubmitted;
       }
       if (!isLoading) {
@@ -69,6 +63,7 @@ const RequestTagModal: React.FC<Props> = ({ imgLink }) => {
               setDescription("");
               setAttach(false);
               setImagePreview("");
+              setImageToBeSubmitted(undefined);
               const text = await response.text();
               if (text === "tag already present/requested") {
                 setTagRequestMessageContent("Tag is already present/requested");
@@ -87,7 +82,7 @@ const RequestTagModal: React.FC<Props> = ({ imgLink }) => {
   );
 
   const handleCloseMessage = useCallback(() => {
-    setShowMessage(false);
+    setShowMessage(false)
     setTagRequestMessageContent("");
     if (
       tagRequestMessageContent ===
@@ -99,127 +94,129 @@ const RequestTagModal: React.FC<Props> = ({ imgLink }) => {
 
   return (
     <>
-      <img src={imgLink} alt={"request-tag"} onClick={handleShow} />
-
-      <Modal className="request-tag-modal" show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Request Tag</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={(e) => submitForm(e)} className="request-tag-form">
-            <div>
-              <label className="form-label">
-                Tag name <strong className="asterisk">*</strong>
-              </label>
-              <input
-                ref={tagInput}
-                type="text"
-                className="form-control"
-                value={tag}
-                onChange={(event) => {
-                  setTag(event.target.value.slice(0, 25));
-                }}
-              />
-            </div>
-
-            <div id="tag-request-icon-preview" className="mt-3">
-              <div>Icon:</div>
-              {imagePreview ? (
-                <img
-                  src={imagePreview}
-                  alt="request-tag-icon"
-                  style={{ height: "25px" }}
+      <div
+        className="request-tag-modal modal show"
+        id="request-tag"
+        style={{ display: "none" }}
+      >
+        <Modal.Dialog>
+          <Modal.Header closeButton onHide={handleClose}>
+            <Modal.Title>Request Tag</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={submitForm} className="request-tag-form">
+              <div>
+                <label className="form-label">
+                  Tag name <strong className="asterisk">*</strong>
+                </label>
+                <input
+                  ref={tagInput}
+                  type="text"
+                  className="form-control"
+                  value={tag}
+                  onChange={(event) => {
+                    setTag(event.target.value.slice(0, 25));
+                  }}
                 />
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="mt-3">
-              <button
-                className="add-image-label"
-                onClick={(e) => {
-                  e.preventDefault();
-                  imageInput.current!.click();
-                }}
-              >
-                <img src="/static/media/add-image-icon.png" alt="add-icon" />
-              </button>
-              <input
-                ref={imageInput}
-                type="file"
-                className="form-control img-input"
-                accept="image/*"
-                onChange={(e) => handleAddImage(e)}
-              />
-            </div>
-            <div className="mt-3">
-              <label htmlFor="tag-request-description" className="form-label">
-                Description
-              </label>
-              <textarea
-                id="tag-request-description"
-                className="form-control"
-                placeholder="Description"
-                value={description}
-                onChange={(event) => {
-                  setDescription(event.target.value.slice(0, 200));
-                }}
-              />
-            </div>
-            <div className="form-check mt-3">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                checked={attach}
-                id="attach-tag-option"
-                onChange={(event) => setAttach(event.target.checked)}
-              />
-              <label className="form-check-label" htmlFor="attach-tag-option">
-                Attach this tag to me once approved
-              </label>
-            </div>
-            <div className="mt-3" id="tag-request-submit-div">
-              <span
-                className="spinner-border text-warning"
-                style={{ display: isLoading ? "" : "none" }}
-              />
-              <button
-                type="submit"
-                className="btn btn-primary"
-                disabled={isLoading}
-              >
-                Request
-              </button>
-            </div>
-            <div
-              className="alert alert-danger mt-3"
-              role="alert"
-              style={{
-                display: errMessage === "" ? "none" : "block",
-              }}
-            >
-              {errMessage}
-            </div>
-          </Form>
+              </div>
 
-          <Modal
-            show={showMessage}
-            onHide={handleCloseMessage}
-            backdrop="static"
-            keyboard={false}
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>Tag request status</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{tagRequestMessageContent}</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleCloseMessage}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </Modal.Body>
-      </Modal>
+              <div id="tag-request-icon-preview" className="mt-3">
+                <div>Icon:</div>
+                {imagePreview && (
+                  <img
+                    src={imagePreview}
+                    alt="request-tag-icon"
+                    style={{ height: "25px" }}
+                  />
+                )}
+              </div>
+              <div className="mt-3">
+                <button
+                  className="add-image-label"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    imageInput.current!.click();
+                  }}
+                >
+                  <img src="/static/media/add-image-icon.png" alt="add-icon" />
+                </button>
+                <input
+                  ref={imageInput}
+                  type="file"
+                  className="form-control img-input"
+                  accept="image/*"
+                  onChange={handleAddImage}
+                />
+              </div>
+              <div className="mt-3">
+                <label htmlFor="tag-request-description" className="form-label">
+                  Description
+                </label>
+                <textarea
+                  id="tag-request-description"
+                  className="form-control"
+                  placeholder="Description"
+                  value={description}
+                  onChange={(event) => {
+                    setDescription(event.target.value.slice(0, 200));
+                  }}
+                />
+              </div>
+              <div className="form-check mt-3">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  checked={attach}
+                  id="attach-tag-option"
+                  onChange={(event) => setAttach(event.target.checked)}
+                />
+                <label className="form-check-label" htmlFor="attach-tag-option">
+                  Attach this tag to me once approved
+                </label>
+              </div>
+              <div className="mt-3" id="tag-request-submit-div">
+                <button
+                  type="submit"
+                  className="btn btn-primary me-3"
+                  disabled={isLoading}
+                >
+                  Request
+                </button>
+                <span
+                  className="spinner-border text-warning"
+                  style={{ display: isLoading ? "" : "none" }}
+                />
+              </div>
+              <div
+                className="alert alert-danger mt-3"
+                role="alert"
+                style={{
+                  display: errMessage === "" ? "none" : "block",
+                }}
+              >
+                {errMessage}
+              </div>
+            </Form>
+
+            <Modal
+              show={showMessage}
+              onHide={handleCloseMessage}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Tag request status</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{tagRequestMessageContent}</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseMessage}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Modal.Body>
+        </Modal.Dialog>
+      </div>
     </>
   );
 };
