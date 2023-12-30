d19ff2ac38bb229efa4f5dbe14b9bdebd475c913
mman <minhmanuni@gmail.com>
2023-12-30 12:18:33 +0700

    copy router file from webfrontend branch to profilesetup branch

--- webfrontend/src/router/router.tsx
+++ webfrontend/src/router/router.tsx
@@ -5,39 +5,33 @@ import { Register } from "../components/auth/register/register";
 import Home from "../pages/home/home";
 import Messages from "../pages/messages/messages";
 import Settings from "../pages/settings/settings";
-import Setup from "../pages/setup/setup";
-
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/messages",
-        element: <Messages />,
-      },
-      {
-        path: "/settings",
-        element: <Settings />,
-      },
-      {
-        path: "/profile/setup",
-        element: <Setup />,
-      },
-    ],
-  },
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/messages",
+                element: <Messages />
+            },
+            {
+                path: "/settings",
+                element: <Settings />
+            }
+        ]
+    }
 ]);
 
 export default router;
