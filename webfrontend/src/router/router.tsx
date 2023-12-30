dbbe4a855c6a1df088a4b933b4faba978d87825f
mman <minhmanuni@gmail.com>
2023-12-30 12:18:48 +0700

    import setup page

--- webfrontend/src/router/router.tsx
+++ webfrontend/src/router/router.tsx
@@ -5,6 +5,7 @@ import { Register } from "../components/auth/register/register";
 import Home from "../pages/home/home";
 import Messages from "../pages/messages/messages";
 import Settings from "../pages/settings/settings";
+import Setup from "../pages/setup/setup";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -29,7 +30,11 @@ const router = createBrowserRouter([
             {
                 path: "/settings",
                 element: <Settings />
-            }
+            },
+            {
+                path: '/profile/setup',
+                element: <Setup />
+            },
         ]
     }
 ]);
